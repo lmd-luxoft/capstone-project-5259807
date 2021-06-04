acdd3ae6dc5ef75bebd6263a54d18eb6e102bed3
alex <5259807@gmail.com>
2021-06-04 18:36:45 +0300

    2

---
+++ HomeAccounting.CompositionRoot/AbstractApplicationBuilder.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeAccounting.CompositionRoot
+{
+    public abstract class AbstractApplicationBuilder
+    {
+        protected readonly IServiceCollection _serviceCollection;
+
+        protected abstract void RegisterBusinessLogic();
+
+        protected abstract void RegisterDataSource();
+
+        protected abstract void RegisterInfrastructure();
+
+        public AbstractApplicationBuilder(IServiceCollection serviceCollection)
+        {
+            _serviceCollection = serviceCollection;
+        }
+
+        public void Build()
+        {
+            RegisterInfrastructure();
+            RegisterDataSource();
+            RegisterBusinessLogic();
+        }
+    }
+}

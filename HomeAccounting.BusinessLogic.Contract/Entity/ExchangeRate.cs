93c3b695f3b9d2549afa7e7b0f3c67e7747d1d64
alex <5259807@gmail.com>
2021-06-10 14:21:49 +0300

    entities

---
+++ HomeAccounting.BusinessLogic.Contract/Entity/ExchangeRate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeAccounting.BusinessLogic.Contract.Entity
+{
+    public class ExchangeRate
+    {
+        public Currency CurrencyIn { get; set; }
+
+        public Currency CurrencyOut { get; set; }
+
+        public decimal Rate { get; set; }
+    }
+}
